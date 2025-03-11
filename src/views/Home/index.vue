bf6bc60aaebb769081b3ef91b2ebe080f4ff4134
Zy1304532694 <1304532694@qq.com>
2025-03-11 14:33:32 +0800

    Home-整体结构和分类的实现

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,13 +1,17 @@
-<template>
-    <div style="height: 1500px;">
-        <h1>Home页面</h1>
-    </div>
-</template>
-
 <script setup>
-
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import homeProduct from './components/HomeProduct.vue'
 </script>
 
-<style scoped>
-
-</style>
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
+</template>
