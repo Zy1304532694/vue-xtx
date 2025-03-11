f9e0f89cf73d930a37db4ccb1765824a745fc604
Zy1304532694 <1304532694@qq.com>
2025-03-11 10:19:00 +0800

    Layout-静态模板搭建

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,10 @@
 </script>
 
 <template>
-  <div class="test">
-    scss test
-  </div>
   <!-- 一级路由出口 -->
   <RouterView />
 </template>
 
 <style scoped lang="scss">
-.test {
-  color: $xtxColor;
-}
+
 </style>
--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,10 +1,15 @@
-<template>
-    <!-- 二级路由出口 -->
-    <RouterView />
-</template>
-
 <script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+
 
 </script>
 
-<style scoped></style>
+<template >
+  <LayoutNav />
+  <LayoutHeader />
+  <!-- 二级路由出口 -->
+  <RouterView />
+  <LayoutFooter />
+</template>
