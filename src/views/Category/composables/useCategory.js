826c4c4529b81a7ebea4f425263b2ccc1ab311c8
Zy1304532694 <1304532694@qq.com>
2025-03-12 13:15:18 +0800

    一级分类-按照业务逻辑拆分函数

---
+++ src/views/Category/composables/useCategory.js
@@ -0,0 +1,23 @@
+// 封装banner轮播图相关代码
+import { getCategoryAPI } from '@/apis/category'
+import { onMounted, ref } from 'vue'
+import { onBeforeRouteUpdate } from 'vue-router'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+
+export function useCategory() {
+  const categoryData = ref({})
+  const getCategory = async (id = route.params.id) => {
+    const res = await getCategoryAPI(id)
+    categoryData.value = res.result
+  }
+  // 获取分类数据
+  onMounted(() => getCategory())
+  onBeforeRouteUpdate((to) => getCategory(to.params.id))
+
+  return {
+    categoryData
+  }
+}
+
--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -1,29 +1,10 @@
 <script setup>
-import { getCategoryAPI } from '@/apis/category'
-import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
-import { getBannerAPI } from '@/apis/home'
 import GoodsItem from '@/views/Home/components/GoodsItem.vue'
-import { onBeforeRouteUpdate } from 'vue-router'
-const route = useRoute()
-const categoryData = ref({})
-const bannerList = ref([])
+import { useBanner } from './composables/useBanner'
+import { useCategory } from './composables/useCategory'
 
-
-const getCategory = async (id = route.params.id) => {
-  const res = await getCategoryAPI(id)
-  categoryData.value = res.result
-}
-onMounted(() => getCategory())
-
-const getBanner = async () => {
-  const res = await getBannerAPI({ distributionSite: 2 })
-  bannerList.value = res.result
-}
-
-onMounted(() => getBanner())
-
-onBeforeRouteUpdate((to) => getCategory(to.params.id))
+const { bannerList } = useBanner()
+const { categoryData } = useCategory()
 
 </script>
 
